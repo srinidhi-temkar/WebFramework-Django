33353f2780f5d6c93d2f119d3e75d84ae86e5798
Srinidhi Temkar <51461263+srinidhi-temkar@users.noreply.github.com>
2020-04-23 03:48:03 +0530

    Put the previous commit into the right folder with slight changes in some files

--- WebFrameworks/urls.py
+++ WebFrameworks/urls.py
@@ -21,7 +21,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Skribbly/', include('Skribbly.urls')),
+    path('', include('Skribbly.urls')),
 ]
 
 if settings.DEBUG:
